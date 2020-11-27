400317bef4096f779e73cac87b4c5d2eb239e481
Marwan Subhi <marwan.subhi4@gmail.com>
2020-11-27 07:57:48 -0500

    added a comment

--- src/Repl/Tasks07/Upcoming.java
+++ src/Repl/Tasks07/Upcoming.java
@@ -4,6 +4,7 @@ public class Upcoming {
     public static void main(String[] args) {
         System.out.println("Good morning");
         //my son will have a good birthday today Insha Alla
+        System.out.println("Today will hopefully practice more");
     }
 }
 
