103c4f937c6ae533dc0f48850c9b766b75121e36
Marwan Subhi <marwan.subhi4@gmail.com>
2020-12-02 16:52:29 -0500

    added new classes

---
+++ src/Repl/Tasks07/ArrayListAddInsert.java
@@ -0,0 +1,18 @@
+package Repl.Tasks07;
+
+import java.util.ArrayList;
+
+public class ArrayListAddInsert {
+    public static void main(String[] args) {
+
+    }
+    public static void test(ArrayList<String> words)
+    {
+        //write code here
+        words.add(0,"hey");
+        words.add(3,"yo");
+
+
+    }
+
+}
