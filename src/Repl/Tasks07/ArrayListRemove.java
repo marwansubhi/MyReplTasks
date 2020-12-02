819a66b4a072f160d420198000ab1f95d2a10dbf
Marwan Subhi <marwan.subhi4@gmail.com>
2020-12-02 16:47:24 -0500

    added new classes

---
+++ src/Repl/Tasks07/ArrayListRemove.java
@@ -0,0 +1,18 @@
+package Repl.Tasks07;
+
+import java.util.ArrayList;
+
+public class ArrayListRemove {
+    public static void main(String[] args) {
+
+
+    }
+    public static void test(ArrayList<Double> dubs)
+    {
+        //write code here
+        dubs.remove(0);
+        dubs.remove(0);
+
+
+    }
+}
