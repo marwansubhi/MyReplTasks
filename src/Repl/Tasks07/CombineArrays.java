e592845d3d0a68b1846d52f103df493200045f91
Marwan <marwan.subhi2002@yahoo.com>
2024-01-14 11:58:28 -0500

    Corrections

--- src/Repl/Tasks07/CombineArrays.java
+++ src/Repl/Tasks07/CombineArrays.java
@@ -1,21 +1,27 @@
 package Repl.Tasks07;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class CombineArrays {
     public static void main(String[] args) {
-        String[] arr1 = {"f","o","o"};
-        String[] arr2 = {"d","e","q"};
-        System.out.println(combineRs(arr1,arr2));
+        String[] arr1 = {"f", "o", "o"};
+        String[] arr2 = {"d", "e", "q"};
+        System.out.println(combineRs(arr1, arr2));
+        String[] combinedArray = combineRs(arr1,arr2).split("");
+        System.out.println(Arrays.toString(combinedArray));
 
 
     }
-    public static String combineRs(String[] r1,String[] r2) {
+
+    public static String combineRs(String[] r1, String[] r2) {
 
         String newString = "";
-        String []arr = new String[r1.length+r2.length];
-        int j =0;
-        for (String each : r1)arr[j++]=each;
-        for (String each : r2)arr[j++]=each;
-        for (String each : arr)newString+=each;
+        String[] arr = new String[r1.length + r2.length];
+        int j = 0;
+        for (String each : r1) arr[j++] = each;
+        for (String each : r2) arr[j++] = each;
+        for (String each : arr) newString += each;
         return newString;
 
     }
